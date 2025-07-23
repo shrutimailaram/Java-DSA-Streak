f2d92e21a8f0b2d1cda590d2e9b718cfce0e7b02
Shruti Mailaram <146420180+shrutimailaram@users.noreply.github.com>
2025-07-24 00:04:31 +0530

    Update StringComparison.java

--- Day02/StringComparison.java
+++ Day02/StringComparison.java
@@ -1,4 +1,10 @@
-           public class StringComparison {
+/* Write a Java program to compare a given string to another string, ignoring case considerations.
+Declare and initialize 3string variables
+Make comparisons of str1 and str2, str2 and str3 using equalsIgnoreCase function
+Store result in bollean variables
+Display the results */
+
+public class StringComparison {
 		    public static void main(String[] args) {
 		        // Declare and initialize string variables
 		        String str1 = "Hello";
