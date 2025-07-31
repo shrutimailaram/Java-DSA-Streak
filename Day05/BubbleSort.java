fd02bec0ea0648bceb4083d8518acd3c580c346a
Shruti Mailaram <shrutigmailaram@acpce.ac.in>
2025-07-31 23:49:43 +0530

    Day:05 - Bubble Sort

---
+++ Day05/BubbleSort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 7, 1, 4};
+        bubbleSort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
